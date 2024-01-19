969bef3c61ba809c4d2143565ecb61e4a795bf0b
inagler <ina.nagler@uib.no>
2024-01-19 12:54:51 +0100

    add sea ice and northward heat and compute annual composites

--- prepare_stf_composites.py
+++ prepare_stf_composites.py
@@ -65,7 +65,6 @@ for i in range(0, len(member_numbers)):
     time = slice(period_start, period_end)
 
     # open files and take annual means 
-    # 3d data
     temp_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/temp/temp_'+member
     ds = xr.open_dataset(temp_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
     
@@ -75,39 +74,6 @@ for i in range(0, len(member_numbers)):
     ds_salt = xr.open_dataset(salt_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
     
     print('SALT file loaded')
-    
-    vvel_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/vvel/vvel_'+member
-    ds_vvel = xr.open_dataset(vvel_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
-    
-    print('VVEL file loaded')
-
-    # 2d data
-    ssh_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/ssh/ssh_'+member
-    ds_ssh = xr.open_dataset(ssh_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
-    
-    print('SSH file loaded')
-
-    shf_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/shf/shf_'+member
-    ds_shf = xr.open_dataset(shf_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
-    
-    print('SHF file loaded')
-    
-    n_heat_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/n_heat/n_heat_'+member
-    ds_n_heat = xr.open_dataset(shf_file).isel(time=time).resample(time='A').mean(dim='time').where(mask3d == 1).roll(nlon=-100)
-    
-    print('N_HEAT file loaded')
-    
-    # march data
-    aice_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/aice/aice_'+member
-    ds_aice = xr.open_dataset(aice_file).sel(time=ds_shf['time.month'] == 3).where(mask3d == 1).roll(nlon=-100)
-    
-    print('AICE file loaded')
-    
-    hmxl_file = '/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/hmxl/hmxl_'+member
-    ds_hmxl = xr.open_dataset(hmxl_file).sel(time=ds_shf['time.month'] == 3).where(mask3d == 1).roll(nlon=-100)
-    
-    print('HMXL file loaded')
-    
 
     # Create final array
     ds = ds.update(ds_salt[["SALT"]])
@@ -119,26 +85,14 @@ for i in range(0, len(member_numbers)):
     # Correct units and attributes
     ds['SIGMA_2'].attrs['units'] = 'kg/m^3 - 1000'
     ds['SIGMA_2'].attrs['long_name'] = 'Potential Density at 2000 dbar'
-
-    ds = ds.update(ds_vvel[["VVEL"]])   
-    ds = ds.update(ds_ssh[["SSH"]])
-    ds = ds.update(ds_shf[["SHF"]])
-    ds = ds.update(ds_n_heat[["N_HEAT"]])
-    ds = ds.update(ds_aice[["AICE"]])
-    ds = ds.update(ds_hmxl[["HMXL"]])
     
     print('SIGMA_2 computed')
-    
-    ds['HMXL'] = ds.HMXL *1e-2
-    ds['SSH'] = ds.SSH *1e-2
-    ds['HMXL'].attrs['units'] = 'm'
-    ds['SSH'].attrs['units'] = 'm'
 
     if 'time_bound' in ds.variables:
         ds = ds.drop_vars('time_bound')
 
     # save array
-    ds.to_netcdf('/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/comp/composite_'+member)
+    ds.to_netcdf('/Data/gfi/share/ModData/CESM2_LENS2/ocean/monthly/comp/stf_composite_'+member)
     
     print(member+' saved')
     print('')
